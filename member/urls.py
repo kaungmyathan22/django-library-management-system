3e48ff3cd63ec605a75a8a7ea549c9382d120605
Kaung Myat Han <arkarhtethan@gmail.com>
2019-08-28 17:54:37 +0630

    Member app crud views added

--- member/urls.py
+++ member/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import MemberCreateView, MemberListView
+from .views import MemberCreateView, MemberListView, MemberUpdateView, member_delete_view
 
 app_name = "member"
 
 urlpatterns = [
-    path('create/',MemberCreateView.as_view(),name='member-create'),
     path('list/',MemberListView.as_view(),name='member-list'),
+    path('create/',MemberCreateView.as_view(),name='member-create'),
+    path('update/<slug:slug>/',MemberUpdateView.as_view(),name='member-update'),
+    path('delete/<int:pk>/',member_delete_view,name='member-delete'),
 ]
--- member/views.py
+++ member/views.py
@@ -1,4 +1,7 @@
-from django.views.generic import CreateView, ListView
+from django.shortcuts import get_object_or_404
+from django.views.generic import CreateView, ListView, UpdateView
+from django.http import JsonResponse
+from django.contrib.auth.decorators import login_required
 from django.urls import reverse_lazy, reverse
 from .models import Member
 from .forms import MemberCreationForm
@@ -15,11 +18,44 @@ class MemberCreateView(LoginRequiredMixin, CreateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['header'] = "Create Member"
+        context['header'] = "Create New Member"
         context['button_name'] = "Create Member"
+        return context
+
+
+class MemberUpdateView(LoginRequiredMixin, UpdateView):
+
+    model = Member
+
+    template_name = "member/form.html"
+
+    form_class = MemberCreationForm
+
+    success_url = reverse_lazy('member:member-list')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['header'] = "Update Member Info"
+        context['button_name'] = "Update Member"
         return context
 
 
 class MemberListView(LoginRequiredMixin, ListView):
 
     model = Member
+
+
+@login_required
+def member_delete_view(request, pk):
+
+    instance = get_object_or_404(Member, pk=pk)
+
+    if request.method == "POST":
+
+        member_name = str(instance)
+
+        instance.delete()
+
+        return JsonResponse({
+            'message': f'Successfully deleted {member_name}.'
+        })
